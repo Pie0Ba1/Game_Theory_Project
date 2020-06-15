44e1d2ca53665bc63a66b00531d0f66b2d112fd8
JakobRobnik <43053552+JakobRobnik@users.noreply.github.com>
2020-06-15 18:21:43 +0200

    Add files via upload

--- brouwer_to_end_of_the_line.py
+++ brouwer_to_end_of_the_line.py
@@ -3,10 +3,11 @@ import matplotlib.pyplot as plt
 import nash_to_brouwer as ntb
 
 def plot_vector_filed(func):
-    N, delta = 30, 0.0001
+    N, delta = 30, 0.01
     plt.figure(figsize = (10, 10))
+    plt.title(ntb.name_game)
 
-    #prepare gric
+    #prepare grid
     xarr = np.linspace(delta, 1-delta, N)
     yarr = np.linspace(delta, 1-delta, N)
     X, Y = np.meshgrid(xarr, yarr)
@@ -19,7 +20,7 @@ def plot_vector_filed(func):
     for i in range(len(Xflat)):
         r[0], r[1] = Xflat[i], Yflat[i]
         D[i] = func(r) - r  #displacement of R after the mapping
-        #D[i] /= np.sqrt(np.sum(np.square(D[i])))
+        D[i] /= np.sqrt(np.sum(np.square(D[i])))
         angle = np.arctan2(D[i, 1], D[i, 0])*2/np.pi
         if angle >= 0 and angle< 1:
             colorarr.append('gold')
@@ -38,10 +39,13 @@ def plot_vector_filed(func):
         plt.plot([xarr[0], xarr[i]], [yarr[i], yarr[0]], lw = lwmy, color = 'grey') #diagonal down lines
         plt.plot([xarr[i], xarr[-1]], [yarr[-1], yarr[i]], lw = lwmy, color = 'grey') #diagonal down lines
 
+    plt.scatter(ntb.Nash_equilibria[:, 0], ntb.Nash_equilibria[:, 1], s = 80, color = 'black')
+
+    plt.savefig('brouwer_to_end_of_the_line' + ntb.name_game)
     plt.show()
 
 
-#plot_vector_filed(ntb.f)
+plot_vector_filed(ntb.f)
 
 
 
